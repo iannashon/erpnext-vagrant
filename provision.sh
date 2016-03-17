fc4e7137afea2a0096c5b142df53ba69eba7e4dc
Valeriy Osipov <info@vosipov.com>
2016-03-17 17:40:25 +0300

    Switched to Semilimes/frappe repository

--- provision.sh
+++ provision.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd $1
-bench init bench --frappe-path https://github.com/osya/frappe.git --apps_path https://raw.githubusercontent.com/frappe/bench/master/install_scripts/erpnext-apps.json
+bench init bench --frappe-path https://github.com/semilimes/frappe.git --apps_path https://raw.githubusercontent.com/frappe/bench/master/install_scripts/erpnext-apps.json
 
 cd $1/bench
 bench new-site erpnext.dev --mariadb-root-password password --admin-password password
